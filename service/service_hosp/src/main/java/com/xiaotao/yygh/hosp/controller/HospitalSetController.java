d0eb6e258f58da980182a2c1789ae8daef05a144
XiaoTao <lzt0212@sina.com>
2022-08-17 15:55:16 +0800

    feat：配置swagger，统一接口返回结果

--- service/service_hosp/src/main/java/com/xiaotao/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/xiaotao/yygh/hosp/controller/HospitalSetController.java
@@ -1,12 +1,16 @@
 package com.xiaotao.yygh.hosp.controller;
 
+import com.xiaotao.yygh.common.result.Result;
 import com.xiaotao.yygh.hosp.service.HospitalSetService;
 import com.xiaotao.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -14,15 +18,22 @@ public class HospitalSetController {
     @Autowired
     private HospitalSetService hospitalSetService;
 
+    @ApiOperation(value = "获取所有医院设置信息")
     @GetMapping("findAll")
-    public List<HospitalSet> list() {
+    public Result<List<HospitalSet>> list() {
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
     }
 
+    @ApiOperation(value = "逻辑删除医院设置信息")
     @DeleteMapping("{id}")
-    public boolean delete(@PathVariable Long id) {
-        return hospitalSetService.removeById(id);
+    public Result delete(@PathVariable Long id) {
+        boolean flag = hospitalSetService.removeById(id);
+        if (flag) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
     }
 
 }
